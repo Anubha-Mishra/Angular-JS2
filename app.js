43f8f1f5a593a6719532ab27222245a9dba55a6f
anubha.mishra <anubha.mishra@digite.com>
2017-04-09 22:52:56 +0530

    Modification

--- app.js
+++ app.js
@@ -9,10 +9,48 @@
   ToBuyController.$inject = ['ShoppingListCheckOffService'];
 
   function ToBuyController(ShoppingListCheckOffService) {
-    
+    var toBuy = this;
+
+    toBuy.items = ShoppingListCheckOffService.showItemsToBuy();
+
+    toBuy.bought = function(index) {
+      ShoppingListCheckOffService.bought(index);
+    };
   }
 
   function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var alreadyBought = this;
 
+    alreadyBought.items = ShoppingListCheckOffService.showItemsBought();
+  }
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    var toBuyList = [
+      {
+        name: "Biscuits",
+        quantity: "10"
+      },
+      {
+        name: "Cookies",
+        quantity: "15"
+      }
+    ];
+
+    var boughtList =[];
+
+    service.showItemsToBuy = function() {
+      return toBuyList;
+    };
+
+    service.bought = function(index) {
+      boughtList.push(toBuyList[index]);
+      toBuyList.splice(index,1);
+    };
+
+    service.showItemsBought = function() {
+      return boughtList;
+    };
   }
 })();
